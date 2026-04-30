e21496e6f6347ca439bd3053d004f23dd347ae6b
Luis Miguel do Carmo <61307237339@espcid.intra>
2026-04-29 21:28:49 -0300

    atualizado com api

---
+++ Aula - 22/index.js
@@ -0,0 +1,66 @@
+console.log("bem vindo aula 22");
+
+function lognumero() {
+  const input = document.getElementById("cep");
+  console.log(`O numero foi: ${input.value.length}`);
+  //para contar o numero de vezes que foi digitado o numero no cep
+
+if (cep.length == 8){
+    console.log('acesso')
+    let inputCEP = document.getElementById('cep')
+    let cep = inputCep.value
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+    fetch (url)
+    .then((resp) => {
+        return resp.json()
+    })
+    .then(dados => {
+        let inputRua = document.getElementById('cep')
+        console.log(dados)
+        inputRua.value = dados.cep
+        inputRua.setAttribute('disabled', 'disabled')
+        
+         let inputLogradouro = document.getElementById('logradouro')
+        console.log(dados)
+        inputRua.value = dados.logradouro
+        inputRua.setAttribute('disable', 'disable') 
+
+        let inputEstado = document.getElementById('estado')
+        console.log(dados)
+        inputRua.value = dados.estado
+        inputRua.setAttribute('disable', 'disable')
+
+        let inputRegiao = document.getElementById('regiao')
+        console.log(dados)
+        inputRua.value = dados.regiao
+        inputRua.setAttribute('disable', 'disable')
+    })
+      .catch(()=>{
+        console.log('erro')
+      })    
+}}
+
+let a = '22'
+
+
+try {
+    
+} catch(cachorro) {
+    console.log(`o erro foi ${cachorro}`)
+}
+
+
+
+
+// ATIVIDADE BUSCAR API
+function buscarApi(){
+  let idBusca = document.getElementById('number').value
+  const url = `https://fakestoreapi.com/products/${idBusca}`
+  fetch(url).then((resp) => {
+    return resp.json()
+  })
+  .then((dados) => {
+    inputNumero = document.getElementById('number')
+    console.log(dados)
+  })
+}
