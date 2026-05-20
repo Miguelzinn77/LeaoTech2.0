7cc130b01094c1103bf104ce256646ecea8bf5c2
Luis Miguel do Carmo <61307237339@espcid.intra>
2026-05-20 19:14:32 -0300

    aula 30

---
+++ aula-30/src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default Login;
