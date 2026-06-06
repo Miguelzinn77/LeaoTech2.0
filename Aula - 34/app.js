5b10762b2ab49b77d2e390ff14aba9c49716c11f
Luis Miguel <luismiguel.151516@gmail.com>
2026-06-06 16:40:07 -0300

    supabase

--- Aula - 34/app.js
+++ Aula - 34/app.js
@@ -7,137 +7,137 @@ const supabase = createClient(
   process.env.SUPABASE_KEY,
 );
 
-// async function inserirAutor(){
+async function inserirAutor(){
 
-// let nome = prompt('digite o nome do autor:')
-// let nacionalidade = prompt('digite a nancionaldade:')
+let nome = prompt('digite o nome do autor:')
+let nacionalidade = prompt('digite a nancionaldade:')
 
-// let novoAutor = {
-//     // nome da esquerda é o nome do banco, na direita é o nome da função
-//     nome: nome,
-//     nacionalidade: nacionalidade,
-// }
+let novoAutor = {
+    // nome da esquerda é o nome do banco, na direita é o nome da função
+    nome: nome,
+    nacionalidade: nacionalidade,
+}
 
-// const { data, error } = await supabase.from('biblioteca_autor').insert(novoAutor).select()
+const { data, error } = await supabase.from('biblioteca_autor').insert(novoAutor).select()
 
-// console.log(data)
-// console.log(error)
+console.log(data)
+console.log(error)
 
-// }
-// inserirAutor()
+}
+inserirAutor()
 
-// async function novoLivro(){
+async function novoLivro(){
 
-// let titulo = prompt('digite o titulo do Livro: ')
-// let quantidade = parseInt(prompt('digite a quantidade: '))
-// let genero = prompt('digite o genero do livro: ')
-// let id_autor = parseInt(prompt('digite o id do autor: '))
+let titulo = prompt('digite o titulo do Livro: ')
+let quantidade = parseInt(prompt('digite a quantidade: '))
+let genero = prompt('digite o genero do livro: ')
+let id_autor = parseInt(prompt('digite o id do autor: '))
 
-// let novoLivro = {
-//     // nome da esquerda é o nome do banco, na direita é o nome da função
-//     titulo:titulo,
-//     quantidade:quantidade,
-//     genero:genero,
-//     id_autor:id_autor
-// }
+let novoLivro = {
+    // nome da esquerda é o nome do banco, na direita é o nome da função
+    titulo:titulo,
+    quantidade:quantidade,
+    genero:genero,
+    id_autor:id_autor
+}
 
-// const { data, error } = await supabase.from('biblioteca_livro').insert(novoLivro).select()
+const { data, error } = await supabase.from('biblioteca_livro').insert(novoLivro).select()
 
-// console.log(data)
-// console.log(error)
+console.log(data)
+console.log(error)
 
-// }
-// novoLivro()
+}
+novoLivro()
 
-// async function novoUsuario() {
-//   let nome = prompt("digite a nome: ");
-//   let telefone = prompt("digite o telefone: ");
-//   let endereco = prompt("endereço: ");
-//   let cpf = prompt("digite o cpf: ");
+async function novoUsuario() {
+  let nome = prompt("digite a nome: ");
+  let telefone = prompt("digite o telefone: ");
+  let endereco = prompt("endereço: ");
+  let cpf = prompt("digite o cpf: ");
 
-//   let novoUsuario = {
-//     // nome da esquerda é o nome do banco, na direita é o nome da função
-//     nome: nome,
-//     telefone: telefone,
-//     endereco: endereco,
-//     cpf: cpf,
-//   };
+  let novoUsuario = {
+    // nome da esquerda é o nome do banco, na direita é o nome da função
+    nome: nome,
+    telefone: telefone,
+    endereco: endereco,
+    cpf: cpf,
+  };
 
-//   const { data, error } = await supabase
-//     .from("biblioteca_usuarios")
-//     .insert(novoUsuario)
-//     .select();
+  const { data, error } = await supabase
+    .from("biblioteca_usuarios")
+    .insert(novoUsuario)
+    .select();
 
-//   console.log(data);
-//   console.log(error);
-// }
-// novoUsuario();
+  console.log(data);
+  console.log(error);
+}
+novoUsuario();
 
-// async function novoPerfil() {
-//   let bio = prompt("digite a bio: ");
-// //   let preferencias = prompt("digite a preferencias: ");
-//   let id_usuario = prompt("digite o id_usuario: ");
-//   let foto = prompt('foto: ')
+async function novoPerfil() {
+  let bio = prompt("digite a bio: ");
+//   let preferencias = prompt("digite a preferencias: ");
+  let id_usuario = prompt("digite o id_usuario: ");
+  let foto = prompt('foto: ')
 
-//   let novoPerfil = {
-//     // nome da esquerda é o nome do banco, na direita é o nome da função
-//     foto:foto,
-//     bio: bio,
-//     // preferencias: preferencias,
-//     id_usuario: id_usuario
+  let novoPerfil = {
+    // nome da esquerda é o nome do banco, na direita é o nome da função
+    foto:foto,
+    bio: bio,
+    // preferencias: preferencias,
+    id_usuario: id_usuario
 
-//   }
+  }
 
-//   const { data, error } = await supabase
-//     .from("biblioteca_perfil")
-//     .insert(novoPerfil)
-//     .select()
+  const { data, error } = await supabase
+    .from("biblioteca_perfil")
+    .insert(novoPerfil)
+    .select()
 
-//   console.log(data)
-//   console.log(error)
-// }
-// novoPerfil();
+  console.log(data)
+  console.log(error)
+}
+novoPerfil();
 
-// async function novoEmprestimo() {
-//   let id_livro = prompt("digite o id_livro: ");
-//   let id_usuario = prompt("digite o id_usuario: ");
-//   let data_entrega = prompt('digite a data_entrega: ')
-//   let data_devolucao = prompt('digite a devolução: ')
+async function novoEmprestimo() {
+  let id_livro = prompt("digite o id_livro: ");
+  let id_usuario = prompt("digite o id_usuario: ");
+  let data_entrega = prompt('digite a data_entrega: ')
+  let data_devolucao = prompt('digite a devolução: ')
 
-//   let novoEmprestimo = {
-//     // nome da esquerda é o nome do banco, na direita é o nome da função
-//     id_livro:id_livro,
-//     id_usuario: id_usuario,
-//     data_entrega: data_entrega,
-//     data_devolucao: data_devolucao
-//   }
+  let novoEmprestimo = {
+    // nome da esquerda é o nome do banco, na direita é o nome da função
+    id_livro:id_livro,
+    id_usuario: id_usuario,
+    data_entrega: data_entrega,
+    data_devolucao: data_devolucao
+  }
 
-//   const { data, error } = await supabase
-//     .from("biblioteca_emprestimo")
-//     .insert(novoEmprestimo)
-//     .select()
+  const { data, error } = await supabase
+    .from("biblioteca_emprestimo")
+    .insert(novoEmprestimo)
+    .select()
 
-//   console.log(data)
-//   console.log(error)
-// }
-// novoEmprestimo();
+  console.log(data)
+  console.log(error)
+}
+novoEmprestimo();
 
-//inserirAutor
+inserirAutor
 
-// async function listarLivros() {
-//   const { data, error } = await supabase
-//     .from("biblioteca_livro")
-//     .select("titulo,genero,  biblioteca_autor(nome, nacionalidade)");
-//   console.log(data);
-//   console.log(error);
+async function listarLivros() {
+  const { data, error } = await supabase
+    .from("biblioteca_livro")
+    .select("titulo,genero,  biblioteca_autor(nome, nacionalidade)");
+  console.log(data);
+  console.log(error);
 
-//   data.forEach((livro) => {
-//     console.log(
-//       `titulo: ${livro.titulo} - ${livro.biblioteca_autor.nome}, genero: ${livro.genero}`,
-//     );
-//   });
-// }
-// listarLivros();
+  data.forEach((livro) => {
+    console.log(
+      `titulo: ${livro.titulo} - ${livro.biblioteca_autor.nome}, genero: ${livro.genero}`,
+    );
+  });
+}
+listarLivros();
 
 
 async function atualizarAutor(id){
